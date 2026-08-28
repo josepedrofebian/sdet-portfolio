ff380ea9346bfc2b01d6a1b3b78eb07133d1e66e
josepedrofebian <josepedro190299@gmail.com>
2026-08-28 17:19:36 +0700

    Menambahkan list belajar method return value

--- 01-java_fundamental/src/main/java/methods/challenges/ReturnValueChallenge.java
+++ 01-java_fundamental/src/main/java/methods/challenges/ReturnValueChallenge.java
@@ -5,35 +5,39 @@ package methods.challenges;
 public class ReturnValueChallenge {
     // Buat method main sebagai titik awal program
     public static void main(String[] args) {
-
+        // Di dalam method main, panggil calculateTestResult
+        // Kirimkan total test dan passed test sebagai argument
+        // Simpan nilai yang dikembalikan method ke dalam variable bernama failedTest
+        int totalFailedTest = calculateTestResult(100, 95);
+        // Tampilkan nilai failedTest ke console
+        System.out.println("Total Failed Test: " + totalFailedTest);
+        // Di dalam method main, panggil getBrowser
+        // Simpan nilai yang dikembalikan ke dalam variable bernama browser
+        String browser = getBrowser();
+        // Tampilkan nilai browser ke console
+        System.out.println(browser);
     }
+
     // Buat method bernama calculateTestResult
     // Gunakan access modifier public
     // Gunakan static
     // Gunakan return type int
     // Buat dua parameter bertipe int: totalTest dan passedTest
-
-    // Hitung jumlah failed test dari totalTest dikurangi passedTest
-
-    // Kembalikan hasil failed test menggunakan return
-
-    // Di dalam method main, panggil calculateTestResult
-    // Kirimkan total test dan passed test sebagai argument
-
-    // Simpan nilai yang dikembalikan method ke dalam variable bernama failedTest
-
-    // Tampilkan nilai failedTest ke console
+    public static int calculateTestResult(int totalTest, int passedTest) {
+        // Hitung jumlah failed test dari totalTest dikurangi passedTest
+        int totalFailedTest = totalTest - passedTest;
+        // Kembalikan hasil failed test menggunakan return
+        return totalFailedTest;
+    }
 
     // Buat method bernama getBrowser
     // Gunakan access modifier public
     // Gunakan static
     // Gunakan return type String
     // Method tidak membutuhkan parameter
+    public static String getBrowser() {
+        // Kembalikan nama browser pilihan lu menggunakan return
+        return "Chrome";
+    }
 
-    // Kembalikan nama browser pilihan lu menggunakan return
-
-    // Di dalam method main, panggil getBrowser
-    // Simpan nilai yang dikembalikan ke dalam variable bernama browser
-
-    // Tampilkan nilai browser ke console
 }
